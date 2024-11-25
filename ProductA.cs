665758a7a368e3f7983d4d0f42964cf54285b50e
vlada-romanchuk23 <V.romanchuk.p752@kfccte-nau.ukr.education>
2024-11-25 15:46:37 +0200

    Add files via upload

---
+++ ProductA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryLibrary
+{
+    public class ProductA : IProduct
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operation of ProductA");
+        }
+    }
+}
